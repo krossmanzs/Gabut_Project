b5f1ffd97dd261048064ccd34946cf7bf040e49f
Cornelius Linux <64464637+krossmanzs@users.noreply.github.com>
2021-02-19 00:53:50 +0700

    Menambahkan keterangan
    Menambahkan keterangan tentang input konstanta dan pembagi

--- src/com/matematika/minat/polinomial/pembagian/PembagianPolinomial.java
+++ src/com/matematika/minat/polinomial/pembagian/PembagianPolinomial.java
@@ -5,10 +5,11 @@ public class PembagianPolinomial {
         // TODO Membuat kode pembagian polinomial
         // 1. Horner kino xd
         // pdf lihat di N:\Mapel\MTK\SEMESTER 2\MINAT\Pembagian_Polinomial.pdf
-
-        int[] konstanta = {2,-6,2,0,-1};
+        
+        // Contoh f(x) = 2x^4-6x^3+2x^2-1
+        int[] konstanta = {2,-6,2,0,-1}; // Masukkan konstanta
         int banyakKonstanta = konstanta.length;
-        int x = 3;
+        int x = 3; // Masukkan pembagi. Contoh: (x + 3)
         Polinomial.Horner(konstanta, banyakKonstanta, x);
 
     }
