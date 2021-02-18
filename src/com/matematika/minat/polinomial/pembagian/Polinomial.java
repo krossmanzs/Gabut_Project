238f2593061c0a3e8d3f889236a9d146e40fff45
Krossmanzs <radi.adi78@gmail.com>
2021-02-19 00:08:05 +0700

    Menambahkan keterangan

--- src/com/matematika/minat/polinomial/pembagian/Polinomial.java
+++ src/com/matematika/minat/polinomial/pembagian/Polinomial.java
@@ -5,20 +5,23 @@ public class Polinomial {
         // inisialisasi string
         StringBuilder fx = new StringBuilder();
 
+        // membentuk fungsi f(x) nya
         for(int i = 0; i < panjangKonstanta; i++){
             if((konstantaHasil[i] != 0) && (konstantaHasil[i] < 0)){
                 fx.append(konstantaHasil[i]);
             } else if ((konstantaHasil[i] != 0) && (konstantaHasil[i] > 0)){
-                if (i != 0) {
+                if (i != 0) { // mencegah konstanta pertama memiliki "+"
                     fx.append("+");
                 }
                 fx.append(konstantaHasil[i]);
             }
 
+            // jika konstanta bernilai 0 maka akan di skip
             if(konstantaHasil[i] == 0){
                 continue;
             }
 
+            // pengkondisian untuk memberikan x nya
             if(i < panjangKonstanta-2){
                 fx.append("x^").append(panjangKonstanta - (i+1));
             } else if(i == panjangKonstanta - 2){
@@ -26,6 +29,7 @@ public class Polinomial {
             }
         }
 
+        // mengubah fx menjadi string dan me return
         return fx.toString();
     }
 
@@ -36,14 +40,15 @@ public class Polinomial {
 
         hasil[0] = konstanta[0];
 
-        // menghitung
+        // menghitung sisa dan hasilnya
         for (int i = 1; i < banyakKonstanta; i++){
-            sisa = (sisa * x) + konstanta[i];
+            sisa = (sisa * x) + konstanta[i]; // rumus mencari sisa
             if(i < banyakKonstanta - 1){
-                hasil[i] = sisa;
+                hasil[i] = sisa; // memasukkan konstanta hasil
             }
         }
 
+        // output
         System.out.println("Hasil: " + bentukFx(hasil,hasil.length));
         System.out.println("Sisa: " + sisa);
     }
